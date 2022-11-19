26d93cfbd6516acbaa2ec5e5556971f5529c3a76
ShowKawazoe <kawazoe.designare@gmail.com>
2022-11-20 05:42:42 +0900

    add order

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import AmbassadorAPIView, ProductGenericAPIView, LinkAPIView
+from .views import AmbassadorAPIView, ProductGenericAPIView, LinkAPIView, OrderAPIView
 
 urlpatterns = [
     path('', include('common.urls')),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('products', ProductGenericAPIView.as_view()),
     path('products/<str:pk>', ProductGenericAPIView.as_view()),
     path('users/<str:pk>/links', LinkAPIView.as_view()),
+    path('orders', OrderAPIView.as_view()),
 ]
