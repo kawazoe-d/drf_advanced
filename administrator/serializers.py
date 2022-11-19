32864d12b68c8e066fd71149aca173ac6faaac38
ShowKawazoe <kawazoe.designare@gmail.com>
2022-11-19 12:29:35 +0900

    add products

---
+++ administrator/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from core.models import Product
+
+# ModelSerializerを継承することでモデルのフィールド定義を再利用可
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        # 対象のモデルクラスを指定
+        model = Product
+        # 利用するモデルのフィールドを指定。全てのフィールドを使用する場合は「__all__」で指定可
+        fields = '__all__'
--- administrator/urls.py
+++ administrator/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from .views import AmbassadorAPIView
+from .views import AmbassadorAPIView, ProductGenericAPIView
 
 urlpatterns = [
     path('', include('common.urls')),
-    path('ambassadors', AmbassadorAPIView.as_view())
+    path('ambassadors', AmbassadorAPIView.as_view()),
+    path('products', ProductGenericAPIView.as_view()),
+    path('products/<str:pk>', ProductGenericAPIView.as_view()),
 ]
