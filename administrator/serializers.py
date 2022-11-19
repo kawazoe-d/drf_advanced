6e670943aac874b2a3a26a955c6c123a7a541d1d
ShowKawazoe <kawazoe.designare@gmail.com>
2022-11-20 05:04:13 +0900

    add links'

--- administrator/serializers.py
+++ administrator/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Product
+from core.models import Product, Link
 
 # ModelSerializerを継承することでモデルのフィールド定義を再利用可
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,4 +8,9 @@ class ProductSerializer(serializers.ModelSerializer):
         # 対象のモデルクラスを指定
         model = Product
         # 利用するモデルのフィールドを指定。全てのフィールドを使用する場合は「__all__」で指定可
+        fields = '__all__'
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
         fields = '__all__'
--- core/models.py
+++ core/models.py
@@ -57,3 +57,10 @@ class Product(models.Model):
     # max_digits 数字部分の最大桁数
     # decimal_places 小数部分の桁数
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Link(models.Model):
+    code = models.TextField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
