176ec9ec05033882212a7d0bf23eb02d28893d92
ShowKawazoe <kawazoe.designare@gmail.com>
2022-11-25 08:43:28 +0900

    add links

--- ambassador/urls.py
+++ ambassador/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import ProductFrontendAPI, ProductBackendAPIView
+from .views import ProductFrontendAPI, ProductBackendAPIView, LinkAPIView
 
 urlpatterns = [
     path('', include('common.urls')),
     path('products/frontend', ProductFrontendAPI.as_view()),
     path('products/backend', ProductBackendAPIView.as_view()),
+    path('links', LinkAPIView.as_view()),
 ]
