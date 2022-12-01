e5f7b0ab458cfc0139634436d996f399b24c537a
ShowKawazoe <kawazoe.designare@gmail.com>
2022-12-01 12:08:39 +0900

    add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,5 @@
+import stripe
+from django.db import transaction
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import exceptions
@@ -14,41 +16,70 @@ class LinkAPIView(APIView):
         return Response(serializer.data)
 
 class OrderAPIView(APIView):
+
+    @transaction.atomic
     def post(self, request):
         data = request.data
         link = Link.objects.filter(code=data['code']).first()
 
         if not link:
-            raise exceptions.APIException('Invalid code')
+            raise exceptions.APIException('Invalid code!')
+        try:
+            order = Order()
+            order.code = link.code
+            order.user_id = link.user.id
+            order.ambassador_email = link.user.email
+            order.first_name = data['first_name']
+            order.last_name = data['last_name']
+            order.email = data['email']
+            order.address = data['address']
+            order.country = data['country']
+            order.city = data['city']
+            order.zip = data['zip']
+            order.save()
 
-        order = Order()
-        order.code = link.code
-        order.user_id = link.user.id
-        order.ambassador_email = link.user.email
-        order.first_name = data['first_name']
-        order.last_name = data['last_name']
-        order.email = data['email']
-        order.address = data['address']
-        order.country = data['country']
-        order.city = data['city']
-        order.zip = data['zip']
-        order.save()
+            line_items = []
 
-        for item in data['products']:
-            product = Product.objects.filter(pk=item['product_id']).first()
-            # decimalモジュール:10進数を扱うためのモジュール
-            # Decimalクラスを用いて10進数データを表現するオブジェクトを作成
-            quantity = decimal.Decimal(item['quantity'])
+            for item in data['products']:
+                product = Product.objects.filter(pk=item['product_id']).first()
+                quantity = decimal.Decimal(item['quantity'])
 
-            order_item = OrderItem()
-            order_item.order = order
-            order_item.product_title = product.title
-            order_item.price = product.price
-            order_item.quantity = quantity
-            order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
-            order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
-            order_item.save()
+                order_item = OrderItem()
+                order_item.order = order
+                order_item.product_title = product.title
+                order_item.price = product.price
+                order_item.quantity = quantity
+                order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
+                order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
+                order_item.save()
+
+                line_items.append({
+                    'name': product.title,
+                    'description': product.description,
+                    'images': [
+                        product.image
+                    ],
+                    'amount': int(100 * product.price),
+                    'currency': 'usd',
+                    'quantity': quantity
+                })
+
+            stripe.api_key = 'sk_test_51H0wSsFHUJ5mamKOVQx6M8kihCIxpBk6DzOhrf4RrpEgqh2bfpI7vbsVu2j5BT0KditccHBnepG33QudcrtBUHfv00Bbw1XXjL'
+
+            source = stripe.checkout.Session.create(
+                success_url='http://localhost:5000/success?source={CHECKOUT_SESSION_ID}',
+                cancel_url='http://localhost:5000/error',
+                payment_method_types=['card'],
+                line_items=line_items
+            )
+
+            order.transaction_id = source['id']
+            order.save()
+
+            return Response(source)
+        except Exception:
+            transaction.rollback()
 
         return Response({
-            'message': 'success'
+            'message': "Error occurred"
         })
